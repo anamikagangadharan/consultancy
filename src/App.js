f941ea704f5e29332533a33745141ca5f8c8464d
alhad-tm <alhadchikkutm@gmail.com>
2023-09-09 13:41:04 +0530

    s/w devp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
+
 import About from './components/About/About';
 import Approach from './components/Approach/Approach';
 import Deliver from './components/Deliver/Deliver';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import SoftwareDevelopment from './components/SoftwareDevelopment/SoftwareDevelopment';
 import Strategy from './components/Strategy/Strategy';
 
 function App() {
   return (
     <div className="App"> 
       <Header/>
-      <Main/>
+      {/* <Main/>
       <About/>
       <Approach/>
       <Strategy/>
       <Deliver/>
-      <Footer/>
+      <Footer/> */}
+      <SoftwareDevelopment/> 
     </div>
   );
 }
---
+++ src/components/SoftwareDevelopment/SoftwareDevelopment.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import css from "./SoftwareDevelopment.module.css"
+
+import D1 from "../../assets/d1.svg"
+
+const SoftwareDevelopment = () => {
+  return (
+    <div className={css.container}>
+      <div className={css.container1}>
+        <div className={css.wrap1}>
+
+            <div className={css.div1}>
+                <span>Choose Invicious for Software Excellence</span>
+                <span>Where Custom Solutions and Innovation Meet. Your trusted partner for software development excellence. In a digital-first world, the right software can transform your business. We're here to make sure it does.</span>
+                <button className={css.startbtn}>Start Project</button>
+            </div>
+
+            </div>
+
+            </div>
+
+<div className={css.container2}> 
+<div className={css.wrap2}>
+
+
+            <div className={css.div2}>
+                <div className={css.div2head}>
+                    <span>Transforming Ideas into Intuitive Experiences</span>
+                </div>
+
+                <div className={css.div2contents}>
+
+                    <div className={css.box}>
+                        <img src={D1} alt="" />
+                        <span>Custom Software Mastery</span>
+                        <span>No one-size-fits-all here. We excel in crafting custom software solutions driven by your unique vision, from concept to execution.</span>
+                    </div>
+                    <div className={css.box}>
+                        <img src={D1} alt="" />
+                        <span>Custom Software Mastery</span>
+                        <span>No one-size-fits-all here. We excel in crafting custom software solutions driven by your unique vision, from concept to execution.</span>
+                    </div>
+                    <div className={css.box}>
+                        <img src={D1} alt="" />
+                        <span>Custom Software Mastery</span>
+                        <span>No one-size-fits-all here. We excel in crafting custom software solutions driven by your unique vision, from concept to execution.</span>
+                    </div>
+                    <div className={css.box}>
+                        <img src={D1} alt="" />
+                        <span>Custom Software Mastery</span>
+                        <span>No one-size-fits-all here. We excel in crafting custom software solutions driven by your unique vision, from concept to execution.</span>
+                    </div>
+                    <div className={css.box}>
+                        <img src={D1} alt="" />
+                        <span>Custom Software Mastery</span>
+                        <span>No one-size-fits-all here. We excel in crafting custom software solutions driven by your unique vision, from concept to execution.</span>
+                    </div>
+                    <div className={css.box}>
+                        <img src={D1} alt="" />
+                        <span>Custom Software Mastery</span>
+                        <span>No one-size-fits-all here. We excel in crafting custom software solutions driven by your unique vision, from concept to execution.</span>
+                    </div>
+                    <div className={css.box}>
+                        <img src={D1} alt="" />
+                        <span>Custom Software Mastery</span>
+                        <span>No one-size-fits-all here. We excel in crafting custom software solutions driven by your unique vision, from concept to execution.</span>
+                    </div>
+                    <div className={css.box}>
+                        <img src={D1} alt="" />
+                        <span>Custom Software Mastery</span>
+                        <span>No one-size-fits-all here. We excel in crafting custom software solutions driven by your unique vision, from concept to execution.</span>
+                    </div>
+
+                </div>
+            </div>
+           
+
+            </div>
+            </div>
+      
+    </div>
+  )
+}
+
+export default SoftwareDevelopment
