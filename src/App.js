e33c2d0d6cf89b829f0a6bdf6d71abd7992e0053
alhad-tm <alhadchikkutm@gmail.com>
2023-09-14 11:44:43 +0530

    added backto-top, favicon,deliverbg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import BackTo from "../src/assets/backtotop.svg" 
+
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -11,11 +13,26 @@ import UiUx from "./components/UiUx/UiUx";
 import Webdevelopment from "./components/Webdevelopment/Webdevelopment";
 import ITconsultancy from "./components/ITconsultancy/ITconsultancy";
 import Mobileapp from "./components/MobileApp/Mobileapp";
+import { useState } from "react";
 
 
 
 
 function App() {
+
+  const [state,setState]=useState(false)
+
+  const backTo=()=>{
+    
+  if (window.scrollY>100){
+    setState(true)
+  } else{
+    setState(false)
+  }
+
+  }
+
+  window.addEventListener("scroll",backTo)
   return (
     <div className="App">
       <Router>
@@ -36,6 +53,7 @@ function App() {
         </Routes>
 
         <Footer /> 
+        <img onClick={()=>window.scrollTo({top: 0,left: 0,behavior: "smooth",})} className= { state? "tobtn2":" tobtn"} src={BackTo} alt=''></img>
 
       </Router>
     </div>
